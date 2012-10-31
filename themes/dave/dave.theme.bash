e942700c4525e549a81f65eca07d270c39c3ae71
David Kaneda <dk@morfunk.com>
2012-10-31 09:43:10 -0700

    Add my theme and copy bash_profile.

---
+++ themes/dave/dave.theme.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+function node_version() {
+    nv=$(node -v) || return
+    echo -e "$RVM_THEME_PROMPT_PREFIX$nv$RVM_THEME_PROMPT_SUFFIX"
+}
+# "${light_blue}$(node_version)  "
+function prompt_command() {
+    PS1="\n${blue}\h ${cyan}— ${light_blue}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${red}→${reset_color} "
+}
+
+PROMPT_COMMAND=prompt_command;
